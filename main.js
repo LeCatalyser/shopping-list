6670832de4eca8e753a61f3eea1c787ae33ac35c
Luisa Salcedo <profe.salcedo@gmail.com>
2017-04-04 10:32:17 -0500

    added javascript code

---
+++ main.js
@@ -0,0 +1,35 @@
+//Requirements:
+// enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+// check and uncheck items on the list by clicking the "Check" button
+// permanently remove items from the list
+
+
+//MCV (MVC)Industry standard
+
+//Model
+var shoppingList = {
+	items: []
+};
+
+//Controler-usualy defining a function. 
+
+// State modification functions
+var addItem = function(state, item) {
+    state.items.push(item);
+};
+
+// Render functions
+var renderList = function(state, element) {
+    var itemsHTML = state.items.map(function(item) {
+        return '<li>' + item + '</li>';
+    });
+    element.html(itemsHTML);
+};
+
+// Event listeners
+$('.shopping-list-add').submit(function(event) {
+    event.preventDefault();
+    addItem(state, $('.shopping-list-add-input').val());
+    renderList(state, $('.shopping-list'));
+});
+
