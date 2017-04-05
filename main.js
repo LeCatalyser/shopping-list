ae370b2b4d833d26bd219f103174f4df8df2cf27
Luisa Salcedo <profe.salcedo@gmail.com>
2017-04-04 19:12:42 -0500

    setting up the code for deleting items off the list

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ var shoppingList = {
 //Controler-usualy defining a function. 
 
 // State modification functions
+//var (variable) addItem
 var addItem = function(state, item) {
     state.items.push(item);
     console.log(state);
@@ -26,7 +27,14 @@ var renderList = function(state, element) {
         return '<li><span class="shopping-item">' + item + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
     });
     element.append(itemsHTML);
+ };
+
+var deleteItems = function(state, element){
+	var itemsHTML = state.items.splice(state, item)
+	delete(shoppingList,)
+	element.delete(itemsHTML);
 };
+
 
 // Event listeners
 $('#js-shopping-list-form').submit(function(event) {
@@ -34,4 +42,9 @@ $('#js-shopping-list-form').submit(function(event) {
     addItem(shoppingList, $('#shopping-list-entry').val());
     renderList(shoppingList, $('.shopping-list'));
 });
+ 
+ //deleteItems.onclick = function(state,element)
+ $(".shopping-item-delete").click(function(){})
 
+//if state.items is an array, you need a way to tell it 
+	//what the index is of the item you want to delete
